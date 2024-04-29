c7e1674e7cf385de609944e104a4019330568725
Massimo Persic <massimo@lloyds-digital.com>
2024-04-29 11:48:29 +0200

    deleted comments

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Get the modal
 var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("uredi_studenta");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 function validateLogin() {
@@ -35,22 +30,18 @@ function addEntry() {
   cellLastName.innerHTML = lastName; 
   cellActions.innerHTML = ' <div class="th-akcije"><img id="uredi_studenta" onclick="editRow(this)" src="./assets/Edit_fill.svg" alt="Edit" width="20" height="20"><img src="./assets/Trash.svg" onclick="deleteRow(this)" alt="Trash" width="20" height="20"></div>'; 
   }
-// Deklaracija globalne varijable selectedRow
 var selectedRow;
 
 function editRow(button) {
   selectedRow = button.parentNode.parentNode.parentNode;
 
-  // Postavljanje vrijednosti unesenih imena i prezimena iz odabranog reda u odgovarajuća input polja za uredivanje
   document.getElementById("edit-ime").value = selectedRow.cells[0].innerHTML;
   document.getElementById("edit-prezime").value = selectedRow.cells[1].innerHTML;
 
-  // Prikazivanje modala za uređivanje (postavljanje display-a na "block")
   document.getElementById("modal").style.display = "block";
 }
 
 function saveChanges() {
-  // Postavljanje vrijednosti unesenih imena i prezimena iz input polja za uređivanje u odabrani redak u tablici
   selectedRow.cells[0].innerHTML = document.getElementById("edit-ime").value;
   selectedRow.cells[1].innerHTML = document.getElementById("edit-prezime").value;
 
@@ -58,15 +49,11 @@ function saveChanges() {
 }
 
 function closeModal() {
-  // Zatvaranje moda (postavljanje display-a na "none")
   document.getElementById("modal").style.display = "none";
 }
 function deleteRow(button) {
-  // Provjerite je li odabran redak za brisanje
   if (selectedRow) {
-      // Pronađite roditeljski element reda i uklonite ga iz tablice
       selectedRow.parentNode.removeChild(selectedRow);
-      // Očistite referencu na odabrani redak
       selectedRow = null;
   } else {
       selectedRow = button.parentNode.parentNode.parentNode;
